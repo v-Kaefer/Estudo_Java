3796b14dedf126de77d53181ccbfa65f6b64d006
v-Kaefer <vitor.guttler@edu.pucrs.br>
2023-04-18 18:05:22 -0300

    Código Final
    Demonstração da Proposta de estudo e do código final (solução).

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # javaProva
- Estudo Java
+ 
+ Estudo da Linguagem Java
+
+ Exercício Avaliativo
+
+Em um baralho de cartas, podemos utilizar uma representação textual para representar os naipes. Eles podem ser representados pelas letras ‘P’, ‘O’, ‘C’ e ‘E’ (Paus, Ouros, Copas e Espadas, respectivamente).
+
+Quanto ao valor da carta, o ‘Ás’ pode ser representado pelo número 1; enquanto as figuras Valete,
+Dama e Rei podem ser representados pelos caracteres ‘J’, ‘Q’ e ‘K’, respectivamente.
+
+Para facilitar, suponha que nosso jogo aceite somente as cartas de Ás até 5, mas todos os naipes devem
+estar presentes no baralho. A carta Ás pode ser representada com o número 1. Os números de 2 a 5 são
+representados como seus números mesmo.
+(Caso queira, pode fazer para todo o baralho, mas não é necessário. Nesse caso, as figuras Valete,
+Dama e Rei podem ser representados pelos números 11, 12 e 13, respectivamente)
+
+
+Seguem alguns exemplos de entradas de dados e seus significados: 
+
+Entrada do usuário -> significado da carta do braralho
+Digite valor: 5 Digite naipe: E -> Cinco de Espada
+Digite valor: 1 Digite naipe: C -> Ás de Copas
+
+**Proposta:**
+
+Faça um programa em Java que solicite ao usuário duas cartas distintas e escreva na tela:
+(a) Se as cartas são do mesmo naipe ou não;
+(b) Se as cartas formam uma dupla (valores iguais, porém naipes diferentes) ou não.
+(c) Se as cartas formam uma sequência simples (independente dos naipes) ou não.
+Exemplos: 4E, 5E || 3E, 4P – valores consecutivos, independente do naipe)
+(d) Se as cartas formam uma sequência premium (mesmos naipes) ou não.
+Exemplos: 1O, 2O – valores consecutivos e do mesmo naipe. 
+
+Observações:
+O programa deverá ser realizado utilizando apenas os recursos aprendidos em aula até o momento.
+Se o usuário entrar com dados inválidos, o programa deverá exibir uma mensagem apropriada e
+encerrar. 
---
+++ cartasNaipes.java
@@ -0,0 +1,113 @@
+// 6° Semana - Exercício Avaliativo
+import java.util.Scanner;
+public class cartasNaipes {
+    public static void main (String[] args) {
+        Scanner input = new Scanner (System.in);                
+        // Input de 2 cartas
+        // 1° Carta
+        System.out.println("Insira o valor da 1° carta.\nDe 1 a 5: ");
+        int valor1 = input.nextInt();
+        if (valor1 < 1 || valor1 >5) {
+            System.out.print("Dado inválido!\nEncerrando Programa.");
+            System.exit(1);
+        }
+
+        // Seleção do Naipe da 1° Carta
+        System.out.println("Insira o naipe da 1° carta. Sendo:\n6 = PAUS, 7 = OUROS, 8 = COPAS & 9 = ESPADAS\n");
+        int naipeA = input.nextInt(); 
+        String naipeASel ="";
+        switch (naipeA) {
+            case 6:
+                naipeASel = "P";
+                break;
+            case 7:
+                naipeASel = "O";
+                break;
+            case 8:
+                naipeASel = "C";
+                break;
+            case 9:
+                naipeASel = "E";
+                break;
+            default:
+                System.out.print("Dado inválido!\nEncerrando Programa.");
+                System.exit(1);
+        }
+        //2° Carta
+        System.out.println("Insira o valor da 2° carta.\nDe 1 a 5: ");
+        int valor2 = input.nextInt();
+        if (valor2 < 1 || valor2 >5) {
+            System.out.print("Dado inválido!\nEncerrando Programa.");
+            System.exit(1);
+        }
+        System.out.println("Insira o naipe da 2° carta. Sendo:\n 6 = PAUS, 7 = OUROS, 8 = COPAS & 9 = ESPADAS\n");
+        int naipeB = input.nextInt();
+        String naipeBSel = "";
+        switch (naipeB) {
+            case 6:
+                naipeBSel = "P" ;
+                break;
+            case 7:
+                naipeBSel = "O" ;
+                break;
+            case 8:
+                naipeBSel = "C" ;
+                break;
+            case 9:
+                naipeBSel = "E" ;
+                break;
+            default:
+                System.out.print("Dado inválido!\n Encerrando Programa.");
+                System.exit(1);
+        }
+        
+        // Display/Print das cartas
+        System.out.println("Sendo as cartas "+valor1+naipeASel+" & "+valor2+naipeBSel+".");
+        System.out.println("Temos, que:");
+        
+        // a) Se são do mesmo naipe:
+        if (naipeASel == naipeBSel) {
+        System.out.print("As cartas são do mesmo naipe ");
+        }
+        else {
+            System.out.print("As cartas NÃO são do mesmo naipe ");
+        }
+        // b) Se formam dupla:
+        if (valor1 == valor2 && naipeASel != naipeBSel) {
+            System.out.println("e formam dupla!");
+        }
+        else if (valor1 == valor2 && naipeASel == naipeBSel) {
+            System.out.println("e NÃO formam dupla!");
+        }
+        else if (valor1 != valor2 && naipeASel == naipeBSel) {
+            System.out.println("e NÃO formam dupla!");
+        }
+        else if (valor1 != valor2 && naipeASel != naipeBSel) {
+            System.out.println("e NÃO formam dupla!");
+        }
+        // c) Se formam sequencia (independente do naipe)
+        if (valor1 == 1 && valor2 == 2) {
+            System.out.print("As cartas formam uma Sequência Simples ");
+        }
+        else if (valor1 == 2 && valor2 == 3) {
+            System.out.print("As cartas formam uma Sequência Simples ");
+        }
+        else if (valor1 == 3 && valor2 == 4) {
+            System.out.print("As cartas formam uma Sequência Simples ");
+        }
+        else if (valor1 == 4 && valor2 == 5) {
+            System.out.print("As cartas formam uma Sequência Simples ");
+        }
+        else {
+            System.out.print("As cartas NÃO formam uma Sequência Simples ");
+        }
+        
+        // d) Se formam uma sequencia premium
+        if (naipeASel == naipeBSel) {
+            System.out.println("e, formam uma Sequência Premium!");
+        }
+        else {
+            System.out.println("mas, NÃO formam uma Sequência Premium!");
+        }
+    }
+}
